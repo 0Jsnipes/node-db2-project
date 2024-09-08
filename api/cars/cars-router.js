69f7a862a809698c4c12cf23fd171cff5c9f5921
0Jsnipes <jared.c.snipes@gmail.com>
2024-09-08 13:40:22 -0400

    done

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,48 @@
-// DO YOUR MAGIC
+const express = require('express');
+const Cars = require('./cars-model');
+const {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+} = require('./cars-middleware'); // Ensure correct import path
+
+const router = express.Router();
+
+// [GET] /api/cars
+router.get('/', async (req, res, next) => {
+  try {
+    const cars = await Cars.getAll();
+    res.status(200).json(cars);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// [GET] /api/cars/:id
+router.get('/:id', checkCarId, async (req, res, next) => {
+  try {
+    const car = await Cars.getById(req.params.id);
+    res.status(200).json(car);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// [POST] /api/cars
+router.post(
+  '/',
+  checkCarPayload, // Make sure this is defined
+  checkVinNumberValid, // Ensure this is defined
+  checkVinNumberUnique, // Ensure this is defined
+  async (req, res, next) => {
+    try {
+      const newCar = await Cars.create(req.body);
+      res.status(201).json(newCar);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+module.exports = router;
